7607202e0a130db7cee28a5c884b8289e05bf93e
SNOWFORSE <134400943+SNOWFORSE@users.noreply.github.com>
2024-09-25 09:24:06 -0300

    Add files via upload

--- grafico/DadosGerais.js
+++ grafico/DadosGerais.js
@@ -1,1 +1,10 @@
 const url = 'https://raw.githubusercontent.com/SNOWFORSE/json/refs/heads/main/grafico.json'
+
+async function vizualizarInformacoes() {
+    const res = await fetch(url)
+    const dados = await res.json()
+
+    console.log(dados)
+}
+
+vizualizarInformacoes()
