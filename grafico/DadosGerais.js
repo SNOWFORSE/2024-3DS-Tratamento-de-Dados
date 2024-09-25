89cb0d6a86888338fafb17961b4253c679485683
SNOWFORSE <134400943+SNOWFORSE@users.noreply.github.com>
2024-09-25 12:06:42 -0300

    Add files via upload

--- grafico/DadosGerais.js
+++ grafico/DadosGerais.js
@@ -3,8 +3,20 @@ const url = 'https://raw.githubusercontent.com/SNOWFORSE/json/refs/heads/main/gr
 async function vizualizarInformacoes() {
     const res = await fetch(url)
     const dados = await res.json()
+    
+    const celularMaisUsado = dados.entrevista.resultados[0].modelo
+    const numeroDePessoasQueUsam = dados.entrevista.resultados[0].votos
+    const porcentagem = dados.entrevista.resultados[0].porcentagem
 
-    console.log(dados)
+    console.log(celularMaisUsado)
+
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('caixa-grafico__texto')
+    paragrafo.innerHTML = `Em uma busca de descobris os melhores custo-beneficio e mais usados em 2024, foi feita uma série de pesquisas em diferentes fontes. Com o auxilio da inteligência artificial do goole, foi possivel estimas que o celular mais usado foi ${celularMaisUsado} com um total de pessoas usando de aproximadamente ${numeroDePessoasQueUsam} com uma porcentagem de ${porcentagem}%`
+
+    const caixa = document.getElementById('caixa-grafico')
+    caixa.appendChild(paragrafo)
+
 }
 
 vizualizarInformacoes()
